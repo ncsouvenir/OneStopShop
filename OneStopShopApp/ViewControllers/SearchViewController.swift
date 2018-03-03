0f9347f30417c92e360f89631f17bce1070a766f
Iram Fattah <iramfattah94@gmail.com>
2018-03-03 10:28:52 -0500

    add files

---
+++ OneStopShopApp/ViewControllers/SearchViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SearchViewController.swift
+//  OneStopShopApp
+//
+//  Created by C4Q on 3/3/18.
+//  Copyright © 2018 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+    
+
+ 
+
+}
