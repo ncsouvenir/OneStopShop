4baeb194adc05710439e8678e9e2923c61c2e182
Iram Fattah <iramfattah94@gmail.com>
2018-03-03 12:24:12 -0500

    configure app delegate

--- OneStopShopApp/AppDelegate.swift
+++ OneStopShopApp/AppDelegate.swift
@@ -16,6 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let tbc = UITabBarController()
+        let searchVC = SearchViewController()
+        let favoritesVC = FavoritesViewController()
+        let navController = UINavigationController(rootViewController: searchVC)
+        tbc.setViewControllers([navController, favoritesVC], animated: true)
+        searchVC.tabBarItem = UITabBarItem(title: "Search", image: nil, tag: 0)
+        favoritesVC.tabBarItem = UITabBarItem(title: "Favorites", image: nil, tag: 1)
+        window?.rootViewController = tbc
+        window?.makeKeyAndVisible()
         return true
     }
 
